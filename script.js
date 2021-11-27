e558bbe4526e92dc59d338992d78c793604a473b
Tony <tcheng@my.bcit.ca>
2021-11-26 21:54:46 -0800

    function createSingleIndex added

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function cleanUpIndex() {
     }
     
 }
+
+function createSingleIndex(name) {
+    const CardLink = document.createElement("a")
+    CardLink.href = "page3.html"
+    const Card = document.Element("div")
+    Card.className = "contact"
+    Card.innerHTML = name
+    CardLink.appendChild(Card)
+    return CardLink
+
+}
