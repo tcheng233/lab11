2dbbdd0eb2817f595c482f6ae5352a831eb7260a
Tony <tcheng@my.bcit.ca>
2021-11-26 22:18:07 -0800

    function renderView created

--- script.js
+++ script.js
@@ -30,3 +30,50 @@ function cleanUpView () {
         main.lastChild.remove()
     }
 }
+
+function renderView (contact) {
+    const contactInfo = document.createElement("div")
+    contactInfo.className = "contactinfo"
+
+    const contactName = document.createElement("div")
+    contactName.className = "contactname"
+    contactName.innerHTML = contact.name
+    const profilePic = document.createElement("img")
+    profilePic.src = "./img/profile.jpg"
+    profilePic.className = "profilepic"
+    profilePic.alt = "Profile picture"
+    contactName.appendChild(profilePic)
+    contactInfo.appendChild(contactName)
+
+    const contactEmail = document.createElement("div")
+    contactEmail.className = "contactemail"
+    contactEmail.innerHTML = "email: " + contact.email
+    contactInfo.appendChild(contactEmail)
+    
+    const contactPhone = document.createElement("div")
+    contactPhone.className = "contactphone"
+    contactPhone.innerHTML = "cell: " + contact.phone
+    contactInfo.appendChild(contactPhone)
+
+    const contactAddress = document.createElement("div")
+    contactAddress.className = "contactaddress"
+    contactAddress.innerHTML = "address: " + contact.address
+    contactInfo.appendChild(contactAddress)
+
+    const btn = document.createElement("div")
+    btn.className = "buttons"
+    const editBtn = document.createElement("button")
+    editBtn.className = "button edit"
+    editBtn.value = "Edit"
+    editBtn.innerHTML = "Edit"
+    btn.appendChild(editBtn)
+    const closeBtn = document.createElement("button")
+    closeBtn.className = "button close"
+    closeBtn.value = "Close"
+    closeBtn.innerHTML = "Close"
+    btn.appendChild(closeBtn)
+
+    contactInfo.appendChild(btn)
+    main.appendChild(contactInfo)
+
+}
